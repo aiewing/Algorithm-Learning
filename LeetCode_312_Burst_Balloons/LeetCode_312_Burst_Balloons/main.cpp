0e79b516f05a78280e7905ad0a55099b87a91bf2
舒毅文 <wb-syw454137@alibaba-inc.com>
2018-11-12 19:51:42 +0800

    1

--- LeetCode_312_Burst_Balloons/LeetCode_312_Burst_Balloons/main.cpp
+++ LeetCode_312_Burst_Balloons/LeetCode_312_Burst_Balloons/main.cpp
@@ -11,15 +11,46 @@
 
 using namespace std;
 
-class Solution {
+// brute-force
+class Solution1 {
 public:
     int maxCoins(vector<int>& nums) {
+        long count = nums.size();
+        // 先找到一个乘积最大的数 然后去掉一个其中最小的数
+        int maxPro = 0;
+        int product = 0;
+        int maxSelectIndex = 0;
+        int minIndex = 0;
+        for (int i = 0; i < count; i++) {
+            if (i == 0) {
+                product = nums[i] * nums[i + 1];
+            } else if (i == count - 1) {
+                product = nums[i] * nums[i - 1];
+            } else {
+                product = nums[i] * nums[i + 1] * nums[i - 1];
+            }
+            if (product > maxPro) {
+                maxSelectIndex = i;
+                maxPro = product;
+            }
+        }
         
+        // 开始去除一个最小的数字
+        if (maxSelectIndex == 0) {
+            
+        }
+        
+        
+        
+        return 0;
     }
 };
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    vector<int> num = {3,1,5,8};
+    Solution1 * so = new Solution1();
+    int res = so->maxCoins(num);
+    cout << res << endl;
     return 0;
 }
