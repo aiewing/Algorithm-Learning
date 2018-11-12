6ba719679ead56aa6c61c786a5a4070f34cacc4d
舒毅文 <wb-syw454137@alibaba-inc.com>
2018-11-12 17:42:19 +0800

    创建算法模版

---
+++ LeetCode_312_Burst_Balloons/LeetCode_312_Burst_Balloons/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  LeetCode_312_Burst_Balloons
+//
+//  Created by 舒毅文 on 2018/11/12.
+//  Copyright © 2018 舒毅文. All rights reserved.
+//
+
+#include <iostream>
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
