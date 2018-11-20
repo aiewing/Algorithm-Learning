c84999423f89b4e06f15a3f070939a36911fa0ab
舒毅文 <wb-syw454137@alibaba-inc.com>
2018-11-20 14:16:51 +0800

    完成冒泡排序，选择排序，插入排序

--- InterviewQuestion/InterviewQuestion_All_Sorting/InterviewQuestion_All_Sorting/main.cpp
+++ InterviewQuestion/InterviewQuestion_All_Sorting/InterviewQuestion_All_Sorting/main.cpp
@@ -7,9 +7,86 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+ 题目
+    列举所有的排序并分析
+ */
+
+
+/// 冒泡排序
+vector<int> sorting1(vector<int> arr) {
+    // 记录是否交换
+    bool flag = true;
+    while (flag) {
+        flag = false;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] < arr[i - 1]) {
+                // 交换
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+                flag = true;
+            }
+        }
+    }
+    return arr;
+}
+
+/// 选择排序
+vector<int> sorting2(vector<int> arr) {
+    int minIndex;
+    for (int i = 0; i < arr.size() - 1; i++) {
+        minIndex = i;
+        for (int j = i + 1; j < arr.size(); j++) {
+            // 先找出一个最小的数
+            minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+        }
+        // 和i - 1位交换
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+    
+    return arr;
+}
+
+/// 插入排序
+vector<int> sorting3(vector<int> arr) {
+    // 已经排好序的位置
+    int sortedIndex = 0;
+    int current;
+    for (int i = 1; i < arr.size(); i++) {
+        current = arr[i];
+        for (int j = sortedIndex; j >= 0; j--) {
+            
+            if (current > arr[j]) {
+                arr[j + 1] = current;
+                sortedIndex++;
+                break;
+            } else if (current < arr[j]) {
+                arr[j + 1] = arr[j];
+                if (j == 0) {
+                    arr[0] = current;
+                    sortedIndex++;
+                    break;
+                }
+            }
+        }
+    }
+    
+    return arr;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<int> arr = {6, 4, 7, 3, 2, 8, 9, 1, 5};
+    vector<int> res = sorting3(arr);
+    for_each(res.begin(), res.end(), [](int a) {
+        cout << a << " ";
+    });
+    cout << endl;
     return 0;
 }
