ca568db52c94dfb4992329a644ca115760f2c0d9
舒毅文 <wb-syw454137@alibaba-inc.com>
2019-03-01 17:05:27 +0800

    LeetCode_746_Min_Cost_Climbing_Stairs

--- LeetCode/LeetCode_746_Min_Cost_Climbing_Stairs/LeetCode_746_Min_Cost_Climbing_Stairs/main.cpp
+++ LeetCode/LeetCode_746_Min_Cost_Climbing_Stairs/LeetCode_746_Min_Cost_Climbing_Stairs/main.cpp
@@ -25,6 +25,23 @@ public:
     }
 };
 
+
+class Solution1 {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int count = cost.size();
+        int a = cost[0];
+        int b = cost[1];
+        int c;
+        for (int i = 2; i < count; i++) {
+            c = min(a, b) + cost[i];
+            a = b;
+            b = c;
+        }
+        return min(a, b);
+    }
+};
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
