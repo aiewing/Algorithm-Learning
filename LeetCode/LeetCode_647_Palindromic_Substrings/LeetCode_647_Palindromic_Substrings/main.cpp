a240ca9ba7c2a478240d3fc186a8fc46985eddfe
舒毅文 <wb-syw454137@alibaba-inc.com>
2019-03-12 19:50:07 +0800

    LeetCode_647_Palindromic_Substrings

--- LeetCode/LeetCode_647_Palindromic_Substrings/LeetCode_647_Palindromic_Substrings/main.cpp
+++ LeetCode/LeetCode_647_Palindromic_Substrings/LeetCode_647_Palindromic_Substrings/main.cpp
@@ -20,16 +20,29 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int count = s.size();
-        vector<int> dp(count + 1, 0);
-        dp[0] = 1;
-        for (int i = 1; i < count; i++) {
-            dp[i] = dp[i - 1] + \
+        int res = 0;
+        vector<vector<bool>> dp(count, vector<bool>(count, false));
+
+        for (int i = count - 1; i >= 0; i--) {
+            for (int j = count - 1; j >= i; j--) {
+                if (s[i] == s[j] && ((j - i) <= 2 || dp[i + 1][j - 1]) ) {
+                    dp[i][j] = true;
+                    res++;
+                }
+            }
         }
+        return res;
     }
 };
 
+/*
+ 昨天说的dp是，dp[i][j] = dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1],这个意思是从中间向两边扩展，我在想，那我直接i置0，那就可以直接从左往右了么？dp[0][j] = dp[0][j - 1] + dp[1][j] - dp[1][j - 1]
+ */
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    Solution * aa = new Solution();
+    int res = aa->countSubstrings("fdsklf");
     std::cout << "Hello, World!\n";
     return 0;
 }
