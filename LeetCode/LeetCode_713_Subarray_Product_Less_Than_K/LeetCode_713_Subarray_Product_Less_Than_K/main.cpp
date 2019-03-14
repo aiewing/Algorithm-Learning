75e07be5e57fa6e2adca841df65e036ea934bcf4
舒毅文 <wb-syw454137@alibaba-inc.com>
2019-03-14 19:43:06 +0800

    LeetCode_713_Subarray_Product_Less_Than_K

--- LeetCode/LeetCode_713_Subarray_Product_Less_Than_K/LeetCode_713_Subarray_Product_Less_Than_K/main.cpp
+++ LeetCode/LeetCode_713_Subarray_Product_Less_Than_K/LeetCode_713_Subarray_Product_Less_Than_K/main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 /*
  超时
  */
-class Solution {
+class Solution1 {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         int count = nums.size();
@@ -46,6 +46,27 @@ public:
             }
         }
         return res;
+    }
+};
+
+/*
+ 滑窗法
+ */
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int prod = 1, ans = 0, left = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            prod *= nums[right];
+            while (prod >= k) {
+                prod /= nums[left++];
+            }
+            ans += right - left + 1;
+        }
+        return ans;
     }
 };
 
