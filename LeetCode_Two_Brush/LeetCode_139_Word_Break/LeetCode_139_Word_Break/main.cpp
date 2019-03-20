fcb41289fab493f2f2cbe99f1ede21211c0a1c35
舒毅文 <wb-syw454137@alibaba-inc.com>
2019-03-20 15:18:17 +0800

    LeetCode_Two_Brush_LeetCode_139_Word_Break

---
+++ LeetCode_Two_Brush/LeetCode_139_Word_Break/LeetCode_139_Word_Break/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  LeetCode_139_Word_Break
+//
+//  Created by 舒毅文 on 2019/3/20.
+//  Copyright © 2019 舒毅文. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                string subStr = s.substr(j, i - j);
+                if (aiewing(subStr, wordDict) && dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        std::cout << "Hello, World!\n";
+        return dp[n];
+    }
+    
+    bool aiewing(string s, vector<string>& wordDict) {
+        for (int i = 0; i < wordDict.size(); i++) {
+            if (s == wordDict[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution *aaa = new Solution();
+    vector<string> bbb = {"leet", "code"};
+    string ccc = "leetcode";
+    bool ddd = aaa->wordBreak(ccc, bbb);
+    std::cout << "Hello, World!\n";
+    return 0;
+}
